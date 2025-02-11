1a3d7b0da991f66d22af76ed1aabd8c021e6d6dc
Adrian-24914 <pen24914@uvg.edu.gt>
2025-02-10 19:24:24 -0600

    test Unitarios para Insertion y Heap Sorts

---
+++ HeapSortTest.java
@@ -0,0 +1,26 @@
+package Hoja_Trabajo_3;
+
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HeapSortTest {
+    private SortAlgorithm heapSort;
+
+    @Test
+    public void testSort_OrdenAleatorio() {
+        heapSort = new HeapSort();
+        List<Integer> entrada = Arrays.asList(4, 10, 3, 5, 1);
+        List<Integer> esperado = Arrays.asList(1, 3, 4, 5, 10);
+        Assert.assertEquals(esperado, heapSort.sort(entrada));
+    }
+    
+    @Test
+    public void testSort_ListaOrdenada() {
+        heapSort = new HeapSort();
+        List<Integer> entrada = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> esperado = Arrays.asList(1, 2, 3, 4, 5);
+        Assert.assertEquals(esperado, heapSort.sort(entrada));
+    }
+}
