a5b9637e239a3a87e095af173fbe1a4fd970ab6b
Adrian-24914 <pen24914@uvg.edu.gt>
2025-02-10 19:32:27 -0600

    Test Unitarios para Quick Sort y Radix Sort

---
+++ RadixSortTest.java
@@ -0,0 +1,29 @@
+package Hoja_Trabajo_3;
+
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RadixSortTest {
+    private SortAlgorithm quickSort;
+    //test unitario para asegurar que el método ordene de forma correcta los datos que se ingresan
+
+    @Test
+    public void testSort_OrdenAleatorio() {
+        quickSort = new RadixSort();
+        List<Integer> entrada = Arrays.asList(4, 10, 3, 5, 1);
+        List<Integer> esperado = Arrays.asList(1, 3, 4, 5, 10);
+        Assert.assertEquals(esperado, quickSort.sort(entrada));
+    }
+    
+    //Si los elementos ya están ordenados, debería no haber ningún cambio y permanecer igual 
+    @Test
+    public void testSort_ListaOrdenada() {
+        quickSort = new RadixSort();
+        List<Integer> entrada = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> esperado = Arrays.asList(1, 2, 3, 4, 5);
+        Assert.assertEquals(esperado, quickSort.sort(entrada));
+    }
+}
+
