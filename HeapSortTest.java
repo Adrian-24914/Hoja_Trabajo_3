0485b1d9064a5a20ba48182fbb992346f9423c5a
Adrian-24914 <pen24914@uvg.edu.gt>
2025-02-10 19:28:53 -0600

    Merge  Sort Test, más comentarios en los test para comprender por qué es que se hacen.

--- HeapSortTest.java
+++ HeapSortTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class HeapSortTest {
     private SortAlgorithm heapSort;
-
+    //test unitario para asegurar que el método ordene de forma correcta los datos que se ingresan
     @Test
     public void testSort_OrdenAleatorio() {
         heapSort = new HeapSort();
@@ -15,7 +15,7 @@ public class HeapSortTest {
         List<Integer> esperado = Arrays.asList(1, 3, 4, 5, 10);
         Assert.assertEquals(esperado, heapSort.sort(entrada));
     }
-    
+    //Si los elementos ya están ordenados, debería no haber ningún cambio y permanecer igual
     @Test
     public void testSort_ListaOrdenada() {
         heapSort = new HeapSort();
--- MergeSortTest.java
+++ MergeSortTest.java
@@ -5,22 +5,23 @@ import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
 
-public class InsertionSortTest {
-    private SortAlgorithm insertionSort;
+public class MergeSortTest {
+    private SortAlgorithm mergeSort;
+
     //test unitario para asegurar que el método ordene de forma correcta los datos que se ingresan
     @Test
     public void testSort_OrdenAleatorio() {
-        insertionSort = new InsertionSort();
+        mergeSort = new MergeSort();
         List<Integer> entrada = Arrays.asList(4, 10, 3, 5, 1);
         List<Integer> esperado = Arrays.asList(1, 3, 4, 5, 10);
-        Assert.assertEquals(esperado, insertionSort.sort(entrada));
+        Assert.assertEquals(esperado, mergeSort.sort(entrada));
     }
-    //Si los elementos ya están ordenados, debería no haber ningún cambio y permanecer igual    
+    //Si los elementos ya están ordenados, debería no haber ningún cambio y permanecer igual 
     @Test
     public void testSort_ListaOrdenada() {
-        insertionSort = new InsertionSort();
+        mergeSort = new MergeSort();
         List<Integer> entrada = Arrays.asList(1, 2, 3, 4, 5);
         List<Integer> esperado = Arrays.asList(1, 2, 3, 4, 5);
-        Assert.assertEquals(esperado, insertionSort.sort(entrada));
+        Assert.assertEquals(esperado, mergeSort.sort(entrada));
     }
 }
